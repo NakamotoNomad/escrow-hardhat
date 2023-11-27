cb3b8908ccbe8b259f566579d93e76a117c08284
NakamotoNomad <nakamotonomad@outlook.com>
2023-11-27 07:57:28 +0700

    Add functionality so the arbiter gets a cut of deposit for their service
    The arbiter now receives 5% of the escrow amount (min 0.001, max 0.1
    ETH) for their service.

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -14,6 +14,16 @@ contract Escrow {
 		_;
 	}
 
+	/*
+	To reward the arbiter for their services (and the gas they have to pay) we need to provide some incentives.
+	The arbiter should receive a small percentage of the escrow amount, with a minimum and maximum cap.
+	The arbiter always gets their reward, no matter if the escrow was approved or denied, so that they don't get any
+	personal benefit either way to increase the chances of fair play.
+	*/
+	uint64 private constant ARBITER_REWARD_MIN_CAP_WEI = 1000000000000000; // 0.001 ETH
+	uint64 private constant ARBITER_REWARD_MAX_CAP_WEI = 100000000000000000; // 0.1 ETH
+	uint8 private constant ARBITER_REWARD_PERCENT = 5;
+
 	event Approved(uint);
 	event Denied(uint);
 
@@ -32,17 +42,36 @@ contract Escrow {
 
 	function approve() external onlyArbiter {
 		uint balance = address(this).balance;
-		(bool sent, ) = payable(beneficiary).call{value: balance}("");
+		uint arbiterCut = calcArbiterCut(balance);
+		uint beneficiaryCut = balance - arbiterCut;
+		(bool sent, ) = payable(beneficiary).call{value: beneficiaryCut}("");
  		require(sent, "Failed to send Ether");
+		(bool sent2, ) = payable(arbiter).call{value: arbiterCut}("");
+		require(sent2, "Failed to send Ether");
 		emit Approved(balance);
 		status = Status.Approved;
 	}
 
 	function deny() external onlyArbiter {
 		uint balance = address(this).balance;
-		(bool sent, ) = payable(depositor).call{value: balance}("");
+		uint arbiterCut = calcArbiterCut(balance);
+		uint depositorRefund = balance - arbiterCut;
+		(bool sent, ) = payable(depositor).call{value: depositorRefund}("");
 		require(sent, "Failed to send Ether");
+		(bool sent2, ) = payable(arbiter).call{value: arbiterCut}("");
+		require(sent2, "Failed to send Ether");
 		emit Denied(balance);
 		status = Status.Denied;
+	}
+
+	function calcArbiterCut(uint value) internal pure returns(uint) {
+		uint cutByPercentage = value * ARBITER_REWARD_PERCENT / 100;
+		if (cutByPercentage < ARBITER_REWARD_MIN_CAP_WEI) {
+			return ARBITER_REWARD_MIN_CAP_WEI;
+		}
+		if (cutByPercentage > ARBITER_REWARD_MAX_CAP_WEI) {
+			return ARBITER_REWARD_MAX_CAP_WEI;
+		}
+		return cutByPercentage;
 	}
 }
--- test/test.js
+++ test/test.js
@@ -7,19 +7,23 @@ describe('Escrow', function () {
   let beneficiary;
   let arbiter;
   const deposit = ethers.utils.parseEther('1');
+
+  async function setupContract(depositAmount) {
+    const Escrow = await ethers.getContractFactory('Escrow');
+    const contract = await Escrow.deploy(
+        arbiter.getAddress(),
+        beneficiary.getAddress(),
+        { value: depositAmount }
+    );
+    await contract.deployed();
+    return contract;
+  }
+
   beforeEach(async () => {
     depositor = ethers.provider.getSigner(0);
     beneficiary = ethers.provider.getSigner(1);
     arbiter = ethers.provider.getSigner(2);
-    const Escrow = await ethers.getContractFactory('Escrow');
-    contract = await Escrow.deploy(
-      arbiter.getAddress(),
-      beneficiary.getAddress(),
-      {
-        value: deposit,
-      }
-    );
-    await contract.deployed();
+    contract = await setupContract(deposit);
   });
 
   it('should be funded initially', async function () {
@@ -41,21 +45,67 @@ describe('Escrow', function () {
 
   describe('after approval from the arbiter', () => {
     it('should transfer balance to beneficiary', async () => {
-      const before = await ethers.provider.getBalance(beneficiary.getAddress());
+      const beforeBalanceBeneficiary = await ethers.provider.getBalance(beneficiary.getAddress());
+      const beforeBalanceArbiter = await ethers.provider.getBalance(arbiter.getAddress());
       const approveTxn = await contract.connect(arbiter).approve();
       await approveTxn.wait();
-      const after = await ethers.provider.getBalance(beneficiary.getAddress());
-      expect(after.sub(before)).to.eq(deposit);
+      const afterBalanceBeneficiary = await ethers.provider.getBalance(beneficiary.getAddress());
+      const afterBalanceArbiter = await ethers.provider.getBalance(arbiter.getAddress());
+
+      expect(afterBalanceBeneficiary.sub(beforeBalanceBeneficiary)).to.eq(ethers.utils.parseEther('0.95'));
+
+      // check the arbiter got their cut. However, as they had to pay gas it'll be slightly less than 0.05
+      expect(afterBalanceArbiter.sub(beforeBalanceArbiter)).to.lt(ethers.utils.parseEther('0.05'));
+      expect(afterBalanceArbiter.sub(beforeBalanceArbiter)).to.gt(ethers.utils.parseEther('0.04'));
+    });
+
+    it('should only transfer a maximum of 0.1 ETH to the arbiter', async () => {
+      const largeDepositContract = await setupContract(ethers.utils.parseEther("100"));
+      const beforeBalanceBeneficiary = await ethers.provider.getBalance(beneficiary.getAddress());
+      const beforeBalanceArbiter = await ethers.provider.getBalance(arbiter.getAddress());
+      const approveTxn = await largeDepositContract.connect(arbiter).approve();
+      await approveTxn.wait();
+      const afterBalanceBeneficiary = await ethers.provider.getBalance(beneficiary.getAddress());
+      const afterBalanceArbiter = await ethers.provider.getBalance(arbiter.getAddress());
+
+      expect(afterBalanceBeneficiary.sub(beforeBalanceBeneficiary)).to.eq(ethers.utils.parseEther('99.9'));
+
+      // the arbiter shouldn't get 5% of the 100 ETH but instead the max cap of 0.1 ETH
+      expect(afterBalanceArbiter.sub(beforeBalanceArbiter)).to.lt(ethers.utils.parseEther('0.1'));
+      expect(afterBalanceArbiter.sub(beforeBalanceArbiter)).to.gt(ethers.utils.parseEther('0.09'));
+    });
+
+    it('should always transfer at least 0.001 ETH to the arbiter', async () => {
+      const smallDepositContract = await setupContract(ethers.utils.parseEther("0.01"));
+      const beforeBalanceBeneficiary = await ethers.provider.getBalance(beneficiary.getAddress());
+      const beforeBalanceArbiter = await ethers.provider.getBalance(arbiter.getAddress());
+      const approveTxn = await smallDepositContract.connect(arbiter).approve();
+      await approveTxn.wait();
+      const afterBalanceBeneficiary = await ethers.provider.getBalance(beneficiary.getAddress());
+      const afterBalanceArbiter = await ethers.provider.getBalance(arbiter.getAddress());
+
+      expect(afterBalanceBeneficiary.sub(beforeBalanceBeneficiary)).to.eq(ethers.utils.parseEther('0.009'));
+
+      // the arbiter shouldn't get only 5% of the 0.01 ETH but instead the min cap of 0.001 ETH
+      expect(afterBalanceArbiter.sub(beforeBalanceArbiter)).to.lt(ethers.utils.parseEther('0.001'));
+      expect(afterBalanceArbiter.sub(beforeBalanceArbiter)).to.gt(ethers.utils.parseEther('0.0009'));
     });
   });
 
   describe('after denial from the arbiter', () => {
     it('should transfer balance to depositor', async () => {
-      const before = await ethers.provider.getBalance(depositor.getAddress());
+      const beforeBalanceDepositor = await ethers.provider.getBalance(depositor.getAddress());
+      const beforeBalanceArbiter = await ethers.provider.getBalance(arbiter.getAddress());
       const approveTxn = await contract.connect(arbiter).deny();
       await approveTxn.wait();
-      const after = await ethers.provider.getBalance(depositor.getAddress());
-      expect(after.sub(before)).to.eq(deposit);
+      const afterBalanceDepositor = await ethers.provider.getBalance(depositor.getAddress());
+      const afterBalanceArbiter = await ethers.provider.getBalance(arbiter.getAddress());
+
+      expect(afterBalanceDepositor.sub(beforeBalanceDepositor)).to.eq(ethers.utils.parseEther('0.95'));
+
+      // check the arbiter got their cut. However, as they had to pay gas it'll be slightly less than 0.05
+      expect(afterBalanceArbiter.sub(beforeBalanceArbiter)).to.lt(ethers.utils.parseEther('0.05'));
+      expect(afterBalanceArbiter.sub(beforeBalanceArbiter)).to.gt(ethers.utils.parseEther('0.04'));
     });
   });
 });
